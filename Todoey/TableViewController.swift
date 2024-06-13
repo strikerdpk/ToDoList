cb656913cb703a1849e0888364c0bc19ed86e1aa
Deepak Kushwaha <kushwaha.deepak788@gmail.com>
2024-06-13 11:36:51 +0530

    Data source is added

--- Todoey/TableViewController.swift
+++ Todoey/TableViewController.swift
@@ -20,27 +20,28 @@ class TableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
+    var data = ["first to do","Second to do","third to do"]
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+       
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return data.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
 
-        // Configure the cell...
-
+         
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
